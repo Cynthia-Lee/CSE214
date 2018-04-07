feb35ceca1fc1067bfe0481b1c6d09de08623881
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2018-04-07 01:12:14 -0400

    Added Homework #5

---
+++ CSE214_Homework5/src/HoleInArrayException.java
@@ -0,0 +1,11 @@
+/**
+ * Exception class that represents when the array would have a hole (space between objects)
+ * 
+ * CSE 214-R12 Recitation, CSE 214-02 Lecture
+ * @author Cynthia Lee
+ * e-mail: cynthia.lee.2@stonybrook.edu
+ * Stony Brook ID: 111737790
+ */
+public class HoleInArrayException extends Exception {
+
+}
