01e8a81b3109ac8751261d41cb49b9abb7e7209e
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2018-05-06 21:48:56 -0400

    Added Homework #7

---
+++ CSE214_Homework7/src/CityDoesNotExistException.java
@@ -0,0 +1,12 @@
+/**
+ * Exception class that represents when a City with that name does not exist.
+ * 
+ * CSE 214-R12 Recitation, CSE 214-02 Lecture
+ * @author Cynthia Lee
+ * e-mail: cynthia.lee.2@stonybrook.edu
+ * Stony Brook ID: 111737790
+ */
+
+public class CityDoesNotExistException extends Exception{
+
+}
